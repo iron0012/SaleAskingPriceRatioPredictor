1c8315602a2c70884b4c4520f90651b44c321c08
Will Tong <will_tong2001@yahoo.com>
2016-06-27 22:36:23 -0700

    will

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ This webapp predicts what the sale price/asking price ratio would be given the f
 2.  Home size (square ft)
 3.  Number of bedrooms
 4.  Number of bathrooms
-5.  Date the home was listed
--- code/predict_price_ratio.py
+++ code/predict_price_ratio.py
@@ -10,7 +10,7 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
-% matplotlib inline
+
 
 WORD_PATTERN = re.compile("(^|\s+)([A-Za-z]+)")
 STEMMER = SnowballStemmer("english")
@@ -46,7 +46,18 @@ def create_dataframe_webapp(zip_codes,
                             vectorizer):
     '''Prepare a pandas dataframe with the input information with the right format for inserting into
     a trained Random Forest model to predict the sale_price to askling_price ratio.
-    input:
+
+    1.  Recreate a dummyfied dataframe of all 71 zip codes in our data called df_zip.
+    2.  Pass the input text (agent's comments) into the the vectorizer transform function.
+        Combine them with the feature names (from the vectorizer object) and create a dataframe
+        called df_text.
+    3.  Create a dataframe that contains the user-input info of: a  number_of_bedrooms
+                                                                 b  number_of_bathrooms
+                                                                 c  home_size_sq_ft
+    4.  Concatenate the above three dataframes and return
+
+
+    Input:
     zip_codes:  a LIST of zip codes that are in random forest model.
     non_text_features: a LIST of text of the names of the non-text features, e.g. 'home_size', 'number_of_bedrooms'.
     additional_stop_words:  a LIST of tokeized_expression that will be removed from the final input matrix.
@@ -60,38 +71,63 @@ def create_dataframe_webapp(zip_codes,
 
     vectorizer: vectorizer to convert input text of the agent's description to the vector of tekenized expressions.
 
+    Output:
+    df: A pandas dataframe that contains all the input data in a format ready to be input into the random forest regressor
+        predictor.
+
+
+
     '''
+    # Recreate a dummyfied dataframe of all 71 zip codes in our data called df_zip.
     df_zip = pd.DataFrame(index = [1], columns = zip_codes)
     df_zip.fillna(0, inplace = True)
 
+    #Pass the input text (agent's comments) into the the vectorizer transform function.
+    #Combine them with the feature names (from the vectorizer object) and create a dataframe called df_text.
     X_stem_lem = vectorizer.transform([input_text])
     features = vectorizer.get_feature_names()
     df_text = pd.DataFrame(X_stem_lem.toarray(), index = [1], columns = features)
     df_text.drop(additional_stop_words, axis = 1, inplace = True)
 
+
+    # Create a dataframe that contains the user-input info of: a  number_of_bedrooms
+    #                                                          b  number_of_bathrooms
+    #                                                          c  home_size_sq_ft
     df_nontxt = pd.DataFrame(index = [1], columns = non_text_features)
-    df_nontxt.set_value(1, non_text_features, [home_size_sq_ft, number_of_bedrooms, number_of_bathrooms])
+    df_nontxt.set_value(1, non_text_features, [number_of_bedrooms, number_of_bathrooms, home_size_sq_ft])
     df_nontxt
 
+    #Concatenate the above three dataframes and return
     df = pd.concat([df_zip, df_text, df_nontxt],
                   axis = 1)
-
     return df
 
 
+'''Load the list of all zip codes used in training the model'''
 filepath = 'list_of_zip_codes'
 zip_codes = pickle.load(open(filepath, 'rb'))
 
-number_of_bedrooms = input('Input the number of bedrooms of the property')
-number_of_bathrooms = input('Input the number of bathrooms of the property')
-zip_code = input('Input the 5-digit zip code of the property')
-zip_code = str(zip_code)
 
-input_text = input("Input the text of the agent's description")
-filepath = 'vectorizer.p'
+'''Query the user on the number_of_bedrooms, number_of_bathrooms, the zip_code,
+and the home_size_sq_ft of the property.'''
+number_of_bedrooms = input('Input the number of bedrooms of the property: ')
+number_of_bathrooms = input('Input the number of bathrooms of the property: ')
+zip_code = input('Input the 5-digit zip code of the property: ')
+zip_code = str(zip_code)
+home_size_sq_ft = input('Input the size of the home in square feet (interior): ')
+
+
+'''Ask the user to input the agent's description of the property.'''
+input_text = raw_input("Input the text of the agent's description (no quotes): ")
+
+
+'''Import the word vectorizer.  Use it to vectorize the agent's comments.'''
+filepath = 'agent_desc_vectorizer.p'
 vectorizer = pickle.load(open(filepath, 'rb'))
 X_stem_lem = vectorizer.transform([input_text])
 
+'''These are additional words that we found would confuse the model and are to
+be removed from the dataframe'''
 additional_stop_words  =  ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'offer',
                            'account', 'disclosur', 'due', 'date', 'sfar', 'broker tour', 'pm', 'offer review',
                            'tour', 'offer date', 'offer due', 'pre', 'accept', 'call', 'close', 'pleas call',
@@ -101,12 +137,29 @@ additional_stop_words  =  ['monday', 'tuesday', 'wednesday', 'thursday', 'friday
                            ]
 non_text_features = ['number_of_bedrooms', 'number_of_bathrooms', 'home_size']
 
-create_dataframe_webapp(zip_codes,
-                        non_text_features,
-                        additional_stop_words,
-                        input_text,
-                        zip_code,
-                        home_size_sq_ft,
-                        number_of_bedrooms,
-                        number_of_bathrooms,
-                        vectorizer)
+'''Prepare a pandas dataframe with the input information with the right format for inserting into
+a trained Random Forest model to predict the sale_price to askling_price ratio.'''
+df =create_dataframe_webapp(zip_codes,
+                            non_text_features,
+                            additional_stop_words,
+                            input_text,
+                            zip_code,
+                            home_size_sq_ft,
+                            number_of_bedrooms,
+                            number_of_bathrooms,
+                            vectorizer)
+
+
+'''Load the trained RandomForestRegressor model'''
+filepath = 'rf_zip_txt_nontxt_model.p'
+rf_final_model_100_50_7 = pickle.load(open(filepath, 'rb'))
+
+
+'''Pass DataFrame (converted to a numpy array) to the RF model to predict the
+price_ratio'''
+price_ratio = rf_final_model_100_50_7.predict(df.values)
+
+'''Output prediction to user'''
+print
+print
+print ('This model estimates the sale price will be ' + str(price_ratio[0])[0:6] + ' times the asking price.')
